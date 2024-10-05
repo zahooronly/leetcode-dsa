7b79baebddb5b801097a21b31e4afc1af8a3e3ab
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-10-05 21:00:19 +0500

    middle node form linkedList problem

---
+++ Easy/LinkedList/middleNode/main.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = head
+        fast = head
+        
+        # Move fast pointer two steps and slow pointer one step at a time
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        return slow
